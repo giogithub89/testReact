1d85f08903a9dc2302174022ac2855cab060390a
giobruno <giobruno@me.com>
2022-11-04 21:56:49 +0100

    first

---
+++ src/Details.js
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from 'react'
+
+export default function Details() {
+
+    const [meal, setMeal] = useState()
+
+
+    const plate = async () =>  {
+        const response = await fetch('https://www.themealdb.com/api/json/v1/1/lookup.php?i=52772)')
+        const json = await response.json()
+        setMeal({...json})
+
+    }
+
+    useEffect(() => {
+        const id = window.history.state.usr.id
+        plate(id)
+      
+      }, [])
+
+  return (
+    <div>
+       { meal && <div>
+       <h1>Titolo: { meal.strMeal}</h1>
+       <h4>Cuisine: {meal.strArea}</h4>
+        <p>Preparazione: { meal.strInstructions}</p>
+        {/* <img src={meal.strMealThumb} ></img> */}
+       </div>}
+
+    </div>
+  )
+}
---
+++ src/Home.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+
+export default function Home() {
+
+    const [searchInput, setSearch] = useState()
+    const [list, setList] = useState()
+
+    const search =  async () =>  {
+        const response = await fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata');
+        const json = await response.json()
+        setList({...json})
+    }
+
+  return (
+    <div>
+        <input placeholder='Search' onChange={(e) => setSearch(e.target.value)} value={searchInput}/>
+        <button onClick={search}>Search</button>
+
+        <div>
+            {list && list.map((item, id) =>{
+                return <Link to={`/${item.idMeal}`}><li key={id}> {item.strMeal} </li></Link>
+            } )}
+        </div>
+
+    </div>
+  )
+}
